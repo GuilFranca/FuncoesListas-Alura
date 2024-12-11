72b2d00f6f0839bf20291d0d347197238969b8d8
Guilherme França <guilfranca27@gmail.com>
2024-12-11 14:18:14 -0300

    Aula 1 - Manipulando Textos

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+// Seleciona a tag no html
+// Associando a variável titulo com a tag selecionada na página HTML
+let titulo = document.querySelector('h1')
+// Dentro do HTML
+titulo.innerHTML = 'Jogo do Número Secreto'
+
+// Associando a variável titulo com a tag selecionada na página HTML
+let paragrafo = document.querySelector('p')
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
