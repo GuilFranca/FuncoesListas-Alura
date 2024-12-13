5099ec2e1930f585a9b7d3d08ee48ab3fba946fa
Guilherme França <guilfranca27@gmail.com>
2024-12-13 16:56:27 -0300

    Aula 02 Funções - Arquivo de aula 3

---
+++ Aulas/js-curso-2-aula3/js-curso-2-aula_-/app.js
@@ -0,0 +1,13 @@
+// Cria uma função para deixar o código mais limpo
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1','Jogo do número secreto')
+exibirTextoNaTela('p','Escolha um número entre 1 e 10')
+
+// Criar função para verificar clique
+function verificarChute() {
+    console.log('O botão foi clicado!');
+}
